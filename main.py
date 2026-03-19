90c9de7455a0744e10f4762738162a60f1020e48
Callum Staras <Callum.staras@googlemail.com>
2026-03-19 14:55:47 +0000

    Start of Project can draw pixels on screen

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+import pygame.gfxdraw
+
+WIDTH = 1920
+HIGHT = 1080
+pygame.init()
+
+screen = pygame.display.set_mode((WIDTH, HIGHT))
+def main():
+
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill("black")
+
+        # Render Here
+        for i in range(HIGHT //2):
+            draw_pixel(i, i, (255, 0, 0))
+
+        # End render
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
+
+def draw_pixel(x, y, color:tuple[int, int, int]):
+    ScreenX = int((WIDTH /2) + x)
+    ScreenY = int((HIGHT /2) - y)
+    pygame.gfxdraw.pixel(screen, ScreenX, ScreenY, color)
+if __name__ == "__main__":
+    main()
