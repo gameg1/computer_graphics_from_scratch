eae1653cec730c55d4448e340ebbc8c3d9ec433d
Callum Staras <Callum.staras@googlemail.com>
2026-03-26 20:52:59 +0000

    Implemented lighting

--- main.py
+++ main.py
@@ -26,14 +26,29 @@ class vector3:
         return vector3(self.x - other.x, self.y - other.y, self.z - other.z)
     
     def __mul__(self, other):
-        if not isinstance(other, int):
-            return NotImplemented
-        return vector3(self.x * other, self.y * other, self.z * other)
+        if isinstance(other, float):
+            return vector3(self.x * other, self.y * other, self.z * other)
+        elif isinstance(other, int):
+            return vector3(self.x * other, self.y * other, self.z * other)
+        return NotImplemented
     
+    def __truediv__(self, other):
+        if isinstance(other, vector3):
+            return vector3(self.x / other.x, self.y / other.y, self.z / other.z)
+        elif isinstance(other, float):
+            return vector3(self.x / other, self.y / other, self.z / other)
+        return NotImplemented
+
     def dot_product(self, other):
         if not isinstance(other, vector3):
             return NotImplemented
         return self.x * other.x + self.y * other.y + self.z * other.z
+    
+    def magnitude(self):
+        return math.sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
+    
+    def normalized(self):
+        return self / self.magnitude()
 
 class sphere:
     def __init__(self, pos:vector3, radius:float, color:tuple[int, int, int]):
@@ -41,6 +56,19 @@ class sphere:
         self.radius = radius
         self.color = color
 
+    def normal(self, position:vector3):
+        return (position - self.pos)/vector3.magnitude(position - self.pos)
+
+class light:
+    def __init__(self, type:str, intensity:float, vec3:vector3 = vector3(0,0,0)):
+        if type not in ["ambient", "point", "directional"]:
+            return NotImplemented
+        self.type:str = type
+        self.intensity:float = intensity
+        if type == "point":
+            self.position:vector3 = vec3
+        elif type == "directional":
+            self.direction:vector3 = vec3
 # Window set up
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 # Camera Info
@@ -55,10 +83,16 @@ viewport_distance = 1
         
 # Set up the envioment
 BACKGROUND_COLOR = (255, 255, 255)
-scene = [
+scene_objects = [
         sphere(vector3(0, -1, 3), 1, (255, 0 , 0)), # Sphere 1
         sphere(vector3(2, 0, 4), 1, (0, 0, 255)),   # Sphere 2
         sphere(vector3(-2, 0, 4), 1, (0, 255, 0)),  # Sphere 3
+        sphere(vector3(0, -5001, 0), 5000, (255, 255, 0))
+        ]
+scene_lights = [
+        light("ambient", 0.2),                      # Ambient light
+        light("point", 0.6, vector3(2, 1, 0)),      # Point light
+        light("directional", 0.2, vector3(1, 4, 4)) # Directional light
         ]
 
 
@@ -109,7 +143,7 @@ def canvas_to_viewport(x:int, y:int):
 def trace_ray(O:vector3, d:vector3, t_min, t_max):
     closest_t = math.inf
     closest_sphere = None
-    for sphere in scene:
+    for sphere in scene_objects:
         t1, t2 = intersect_ray_sphere(O, d, sphere)
         if t1 < closest_t and t_min < t1 < t_max:
             closest_t = t1
@@ -119,7 +153,18 @@ def trace_ray(O:vector3, d:vector3, t_min, t_max):
             closest_sphere = sphere
     if closest_sphere == None:
         return BACKGROUND_COLOR
-    return closest_sphere.color
+    
+    point = O + d * closest_t  # Compute intersection
+    normal = closest_sphere.normal(point)
+    normal = normal.normalized()
+    #print(closest_sphere.color)
+    #print(compute_lighting(point, normal))
+    r, g, b = closest_sphere.color
+    c_light = compute_lighting(point, normal)
+    r = int(r * c_light)
+    g = int(g * c_light)
+    b = int(b * c_light)
+    return (r, g, b)
     
 def intersect_ray_sphere(O:vector3, D:vector3, sphere:sphere):
     r = sphere.radius
@@ -137,6 +182,22 @@ def intersect_ray_sphere(O:vector3, D:vector3, sphere:sphere):
     t2 = (-b - math.sqrt(discriminant)) / (2 * a)
 
     return t1, t2
+
+def compute_lighting(point:vector3, normal:vector3):
+    i:float = 0.0
+    for light in scene_lights:
+        if light.type == "ambient":
+            i += light.intensity
+        else:
+            if light.type == "point":
+                L = light.position - point
+            else:
+                L = light.direction
+            # Check here if something goes wrong - indentation
+            n_dot_l = vector3.dot_product(normal, L)
+            if n_dot_l > 0:
+                i += light.intensity * n_dot_l / (normal.magnitude() * L.magnitude())
+    return i
 
 
 def clamp (n:int):
